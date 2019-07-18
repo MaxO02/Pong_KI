fc367966f86a081176c05d3f1c131846ed1e2509
osswald.max@gmail.com <Schnufacdcgitfel123>
2019-07-18 16:59:31 +0200

    we finaly have been able to create a standalone

--- setup_Standalone.py
+++ setup_Standalone.py
@@ -4,14 +4,14 @@ import os
 os.environ['TCL_LIBRARY'] = "C:\\LOCAL_TO_PYTHON\\Python35-32\\tcl8.6"
 os.environ['TK_LIBRARY'] = "C:\\LOCAL_TO_PYTHON\\Python35-32\\tk8.6"
 
-executables = [cx_Freeze.Executable("ALLINONE.py")]
+executables = [cx_Freeze.Executable("__init__.py")]
 
 cx_Freeze.setup(
     name="Pong_clone",
     options = {
         "build_exe":{
-            "packages":["pygame","multitasking"],
-            "include_files": ["soundfiles","config.cfg"]
+            "packages":["pygame","multitasking","configparser"],
+            "include_files": ["Pong","Pong/config.cfg","Pong/soundfiles"]
         }
     },
     executables = executables
